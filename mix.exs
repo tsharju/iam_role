68b67feebca8ee3a6b7082a05f5a3874e39af53a
Teemu Harju <teemu.harju@gmail.com>
2015-10-27 09:49:31 +0200

    Updated mix.exs.

--- mix.exs
+++ mix.exs
@@ -3,33 +3,35 @@ defmodule IamRole.Mixfile do
 
   def project do
     [app: :iam_role,
-     version: "0.0.1",
+     version: "1.0.0",
+     description: "Application for automatically fetching AWS IAM " <>
+       "role security credentials.",
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     package: package,
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger, :inets, :jsone],
      mod: {IamRole, []}]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
+      {:earmark, "~> 0.1", only: :dev},
+      {:ex_doc, "~> 0.10", only: :dev},
       {:jsone, "~> 1.2"}
     ]
   end
+
+  defp package do
+    [
+      maintainers: ["Teemu Harju"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/tsharju/iam_role"}
+    ]
+  end
+  
 end
